5585b7b39f49ce6cc8cc6183a61f1bc2cac96c98
mkoch <matthias.koch@smail.inf.h-brs.de>
2024-05-14 13:51:58 +0200

    run.sh: Runs without chosen frequency now. Updated info output

--- run.sh
+++ run.sh
@@ -2,12 +2,13 @@
 
 set -eu
 
+FREQUENCY=${1:-}
 # runs the docker container with the correct parameters
 # check if parameters are provided
 if [ $# -eq 0 ]; then
-    echo "No arguments provided. Run with frequency, check https://www.cellmapper.net/arfcn for conversion of ARFCN to
-     frequency. Example for arfcn=1016: ./run.sh 933.4M"
-    exit 1
+    echo "No arguments provided. GSM-Monitor will search for GSM signals in the 900MHz band."
+    echo "To run with specific frequency/arfcn, check https://www.cellmapper.net/arfcn for 
+    conversion of ARFCN to frequency. Example for arfcn=1016: ./run.sh 933.4M"
 fi
 
 # Use sudo if docker is not available to the current user
@@ -18,13 +19,15 @@ else
 fi
 
 # build the docker image from the Dockerfile
+echo "Building docker image..."
 $DOCKER build -t gsm-monitor .
 
 # run the docker image with the correct parameters and remove the container after it is done
 # mount directory for output files
+echo "Running docker image..."
 $DOCKER run -it --privileged \
   -v /etc/timezone:/etc/timezone:ro \
   -v /etc/localtime:/etc/localtime:ro \
   -v /dev/bus/usb:/dev/bus/usb \
-  -v "$(pwd)/output:/output" \
+  -v "/home/$USER/output:/output" \
   gsm-monitor "$@"
